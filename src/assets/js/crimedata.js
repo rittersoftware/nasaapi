975c20a84173e293ea3dfa3b254d52aadc44d314
Jim Ritter <jritter@rittersoftware.com>
2018-08-03 07:58:59 -0400

    initialcommit
    Initial Commit

---
+++ src/assets/js/crimedata.js
@@ -0,0 +1,9 @@
+let unirest = require('unirest');
+
+// These code snippets use an open-source library. http://unirest.io/nodejs
+unirest.get("https://jgentes-Crime-Data-v1.p.mashape.com/crime?enddate=9%2F25%2F2015&lat=37.757815&long=-122.5076392&startdate=9%2F19%2F2015")
+    .header("X-Mashape-Key", "WLCAyFwkESmshwFqB4kQPcbiYRepp1vdxkhjsnAS1dm23p90HE")
+    .header("Accept", "application/json")
+    .end(function (result) {
+        console.log(result.body);
+    });
